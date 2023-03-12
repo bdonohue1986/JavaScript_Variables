3b6827d5e5d63d79865516ae6b1df06afc08cf98
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2023-03-12 07:58:47 -0400

    completed

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+const FullName = firstname + + lastName;
+const firstname = "Byron";
+let lastName = "Donohue";
+var age = 37;
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage =15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+let title = "Name Of The Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(" Hi my name is " + FullName +". " );
+console.log("I've been reading this book "+ author + " it's called" + title +"." );
 
 // Exercise 1
-
+ 
 // Exercise 2
 
 // Exercise 3
